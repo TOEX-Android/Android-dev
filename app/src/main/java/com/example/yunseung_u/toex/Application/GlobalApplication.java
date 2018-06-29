8755b0e6a5026c51a6c845a75dd81a5054f81e89
윤승우 <yhnu191919@gmail.com>
2018-06-30 01:20:48 +0900

    MainAcitivity UI

--- app/src/main/java/com/example/yunseung_u/toex/Application/GlobalApplication.java
+++ app/src/main/java/com/example/yunseung_u/toex/Application/GlobalApplication.java
@@ -3,15 +3,23 @@ package com.example.yunseung_u.toex.Application;
 import android.app.Activity;
 import android.app.Application;
 
+import com.example.yunseung_u.toex.model.UserInfo;
 import com.example.yunseung_u.toex.view.adapter.KakaoSDKAdapter;
 import com.kakao.auth.KakaoSDK;
+
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
 
 public class GlobalApplication extends Application {
 
 
 
+    private static Retrofit retrofit = null;
     private static volatile GlobalApplication obj = null;
     private static volatile Activity currentActivity = null;
+    private static UserInfo currentUser = new UserInfo();
+
+
 
     @Override
     public void onCreate() {
@@ -28,6 +36,19 @@ public class GlobalApplication extends Application {
         return currentActivity;
     }
 
+
+    public static Retrofit getRetrofit(String baseURL)
+    {
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+
     // Activity가 올라올때마다 Activity의 onCreate에서 호출해줘야한다.
     public static void setCurrentActivity(Activity currentActivity) {
         GlobalApplication.currentActivity = currentActivity;
@@ -37,5 +58,13 @@ public class GlobalApplication extends Application {
     public void onTerminate() {
         super.onTerminate();
         obj = null;
+    }
+
+    public static UserInfo getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(UserInfo userInfo) {
+        GlobalApplication.currentUser = userInfo;
     }
 }
